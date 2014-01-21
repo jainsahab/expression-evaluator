3552e2353c40559f02c7cc2bad5d36ef634f80c2
Prateek <Prateekj@thoughtworks.com>
2014-01-21 19:56:27 +0530

    Refactored a method

--- src/prateekj/expressionEvaluator/Evaluator.java
+++ src/prateekj/expressionEvaluator/Evaluator.java
@@ -1,24 +1,45 @@
 package prateekj.expressionEvaluator;
 
-import java.util.Stack;
+import java.util.*;
+
+interface Operation {
+    double operation(double Operand1,double Operand2);
+}
 
 public class Evaluator {
-    double addOperands(Double Operator1,Double Operator2){
-        return  Operator1+Operator2;
-    }
-    double subtractOperands(Double Operator1,Double Operator2){
-        return  Operator1-Operator2;
-    }
-    double productOfOperands(Double Operator1, Double Operator2) {
-        return Operator1*Operator2;
-    }
-
-    double divisionOfOperands(Double Operator1, Double Operator2) {
-        return Operator1/Operator2;
-    }
-
-    double powerOfOperands(Double Operator1, Double Operator2) {
-        return Math.pow(Operator1,Operator2);
+    private Map<Character, Operation> createMap() {
+        Map<Character, Operation> methodMap = new HashMap<Character, Operation>();
+        methodMap.put('+',new Operation() {
+            @Override
+            public double operation(double operand1, double operand2) {
+                return  operand1+operand2;
+            }
+        });
+        methodMap.put('-',new Operation() {
+            @Override
+            public double operation(double operand1, double operand2) {
+                return  operand1-operand2;
+            }
+        });
+        methodMap.put('*',new Operation() {
+            @Override
+            public double operation(double operand1, double operand2) {
+                return  operand1*operand2;
+            }
+        });
+        methodMap.put('/',new Operation() {
+            @Override
+            public double operation(double operand1, double operand2) {
+                return  operand1/operand2;
+            }
+        });
+        methodMap.put('^',new Operation() {
+            @Override
+            public double operation(double operand1, double operand2) {
+                return  Math.pow(operand1,operand2);
+            }
+        });
+        return methodMap;
     }
 
     boolean isOperator(String expressionPart) {
@@ -26,40 +47,30 @@ public class Evaluator {
         return possibleOperators.contains(expressionPart);
     }
 
-    public Double evaluateExpression(Stack operands, Stack operators) {
+    public Double evaluateExpression(Stack operands, Stack operators,Map<Character, Operation> methodMap) {
         Double result = null;
         while(operands.size() != 1){
             Double Operator1 = (Double) operands.pop();
             Double Operator2 = (Double) operands.pop();
-            String operator = (String) operators.pop();
-            if(operator.equals("+"))
-                result = addOperands(Operator1,Operator2);
-            if(operator.equals("-"))
-                result =  subtractOperands(Operator1, Operator2);
-            if(operator.equals("*"))
-                result =  productOfOperands(Operator1, Operator2);
-            if(operator.equals("/"))
-                result =  divisionOfOperands(Operator1, Operator2);
-            if(operator.equals("^"))
-                result =  powerOfOperands(Operator1, Operator2);
+            Character operator = (Character) operators.pop();
+            result = methodMap.get(operator).operation(Operator1, Operator2);
             operands.push(result);
         }
         return result;
     }
 
     public double getEvaluated(String expression){
+        Map<Character, Operation> methodMap = createMap();
         Stack operands = new Stack();
-        Stack operators = new Stack();
+        Stack<Character> operators = new Stack<Character>();
         String[] expressionParts = expression.split(" ");
         for (int index=expressionParts.length-1 ; index >= 0 ; index--) {
-            if(isOperator(expressionParts[index])) {
-                operators.push(expressionParts[index]);
-            }
-            else {
+            if(isOperator(expressionParts[index]))
+                operators.push((expressionParts[index].charAt(0)));
+            else
                 operands.push(Double.parseDouble(expressionParts[index]));
-            }
         }
-        Double result = evaluateExpression(operands, operators);
+        Double result = evaluateExpression(operands, operators,methodMap);
         return result;
     }
 }
